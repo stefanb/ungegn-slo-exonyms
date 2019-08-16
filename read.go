cdcc958a3e56b97d27576715483a05ce26de76f1
Stefan Baebler <stefan.baebler@gmail.com>
2019-08-16 04:43:46 +0200

    read to struct

--- read.go
+++ read.go
@@ -14,11 +14,29 @@ func main() {
 		log.Fatal(err)
 	}
 	for _, sheet := range xlFile.Sheets {
-		for _, row := range sheet.Rows {
-			for _, cell := range row.Cells {
-				text := cell.String()
-				fmt.Printf("%s\n", text)
+		for i, row := range sheet.Rows {
+			if i == 0 || row.Cells[0].Value == "" {
+				continue
 			}
+			ex := &Exonym{}
+			err := row.ReadStruct(ex)
+			if err != nil {
+				log.Fatal("Error reading to struct:", err)
+			}
+			fmt.Println(ex)
 		}
 	}
+}
+
+type Exonym struct {
+	ID          int     `xlsx:"0"`
+	NameSl      string  `xlsx:"1"`
+	NameOrig    string  `xlsx:"4"`
+	LangOrig    string  `xlsx:"5"`
+	FeatureType string  `xlsx:"8"`
+	LatDMS      string  `xlsx:"9"`
+	Lat         float64 `xlsx:"-"`
+	LonDMS      string  `xlsx:"10"`
+	Lon         float64 `xlsx:"-"`
+	//BoolVal bool `xlsx:"4"`
 }
