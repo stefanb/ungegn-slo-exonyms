3673af630d49443951fb61904245c721bf770abe
Stefan Baebler <stefan.baebler@gmail.com>
2020-01-29 22:07:12 +0100

    refactor: isolated ParseDMS

--- read.go
+++ read.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"math"
 
 	//	"fmt"
 	"io/ioutil"
 	"log"
-	"strconv"
 	"strings"
-	"unicode/utf8"
 
 	"github.com/tealeg/xlsx"
 
@@ -138,55 +134,4 @@ func setOptionalProperty(f *geojson.Feature, key string, value string) {
 
 func hasValue(in string) bool {
 	return in != "" && in != "–" && in != "0" && in != "ni" && in != "???" && strings.TrimSpace(in) != ""
-}
-
-func ParseDMS(dms string) (float64, error) {
-	if !hasValue(dms) {
-		return 0, errors.New("No dms value to parse: " + strconv.Quote(dms))
-	}
-
-	dmsIn := dms
-	dms = strings.ReplaceAll(dms, "\u00b0", "\u00b0 ")
-	dms = strings.ReplaceAll(dms, "\u2032", "\u2032 ")
-	dms = strings.ReplaceAll(dms, "\u2033", "\u2033 ")
-	d, m, s, q := 0, 0, 0, 1
-	errMsg := ""
-	for _, part := range strings.Fields(dms) {
-		if len(part) == 1 {
-			if part == "J" || part == "Z" {
-				q = -1
-			}
-			continue
-		}
-
-		unit, ulen := utf8.DecodeLastRuneInString(part)
-		if len(part) == ulen {
-			errMsg = errMsg + "No value to parse from: " + part + " in: " + dmsIn
-			continue
-		}
-		val, err := strconv.Atoi(part[:len(part)-ulen])
-		if err != nil {
-			errMsg = errMsg + "Error parsing: " + strconv.Quote(part) + " from: " + strconv.Quote(dmsIn) + " reason: " + err.Error()
-			continue
-		}
-
-		switch unit {
-		case 176: //'\u00b0':
-			d = val
-		case 8242: //'\u2032':
-			m = val
-		case 8243: //'\u2033':
-			s = val
-		default:
-			errMsg = errMsg + "Unknown angle unit:" + string(unit)
-		}
-	}
-
-	var err error
-	if errMsg != "" {
-		err = errors.New(errMsg)
-	}
-
-	deg := float64(q) * (float64(d) + float64(m)/60 + float64(s)/60/60)
-	return math.Round(deg*10000) / 10000, err
 }
